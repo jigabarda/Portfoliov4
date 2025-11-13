fc2d770ceadb6a529a254ba67c2c7b64d700913d
James Ivan Gabarda <jamesivangabarda8@gmail.com>
2025-11-13 19:01:50 +0800

    update styling to the latest version and its ui/ux

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,44 +1,56 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white px-8 py-10">
+  <footer className="text-white px-8 py-10">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-8 max-w-6xl mx-auto text-sm">
         {/* Logo */}
-        <div>
-          <h1 className="text-2xl font-extrabold footer-logo">JIGSTACK</h1>
+      <div className="flex-1 flex items-center h-full">
+        <div className="flex items-center justify-center -ml-4">
+          <Image
+            src={"/images/logo2.png"}
+            alt="Profile"
+            width={150}
+            height={150}
+          />
         </div>
+      </div>
 
-        {/* Navigation */}
-        <div>
-          <h4 className="text-[#A30000] font-semibold mb-2">Navigation</h4>
-          <ul className="space-y-1">
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Stack</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-
-        {/* Follow Me */}
-        <div>
-          <h4 className="text-[#A30000] font-semibold mb-2">Follow Me</h4>
-          <ul className="space-y-1">
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>LinkedIn</li>
-          </ul>
+        {/* Navigation & Follow Me (2 columns on mobile) */}
+        <div className="col-span-2 md:col-span-1 ">
+            <div className="grid grid-cols-2 md:gap-30">
+            {/* Navigation */}
+            <div>
+              <h4 className="text-[#A30000] font-semibold mb-2">Navigation</h4>
+              <ul className="space-y-1">
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Stack</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            {/* Follow Me */}
+            <div>
+              <h4 className="text-[#A30000] font-semibold mb-2 md:whitespace-nowrap">Follow Me</h4>
+              <ul className="space-y-1">
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>LinkedIn</li>
+              </ul>
+            </div>
+          </div>
         </div>
 
         {/* Let's Connect */}
-        <div>
-          <h4 className="text-[#A30000] font-semibold mb-2">Let’s Connect</h4>
+        <div className=" md:ml-20 md:whitespace-nowrap">
+          <h4 className="text-[#A30000] font-semibold mb-2 ">Let’s Connect</h4>
           <p className="mb-2 underline">jagabarda8@gmail.com</p>
           <input
             type="email"
             placeholder="Your email"
-            className="bg-black border border-white text-white px-3 py-1 w-full"
+            className="bg-black border border-white text-white px-3 py-1 w-full md:w-50 lg:w-50"
           />
         </div>
       </div>
@@ -50,8 +62,8 @@ const Footer = () => {
 
       {/* Scroll Up Button */}
       <div className="fixed bottom-6 right-6">
-        <button className="bg-white text-[#A30000] p-2 rounded-full hover:bg-[#A30000] hover:text-white transition">
-          ↑
+        <button className="bg-white text-[#A30000] p-3 rounded-full hover:bg-[#A30000] hover:text-white transition">
+          ▲
         </button>
       </div>
     </footer>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useState } from "react";
+import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 import "@fontsource/anton";
 import "@fontsource/secular-one";
+import Image from "next/image";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,13 +24,21 @@ const Navbar = () => {
 
   return (
     <div
-      className={`w-full flex items-center justify-between fixed top-0 z-50 transition-all duration-500 ${
-        scrolled ? "bg-white/10 p-2 shadow-lg" : "bg-transparent"
-      } sm:mt-0`}
+      className={`w-full flex flex-row items-center justify-between fixed top-0 z-50 transition-all duration-500 ${
+        scrolled ? "bg-black/70 p-2 shadow-lg" : "bg-transparent"
+      } sm:mt-0 h-15`}
     >
-      <h1 className="text-[#E50914] text-[15px] font-black drop-shadow-[0_2px_2px_rgba(0,0,0,0.25)] font-secular-one uppercase">
-        JIGSTACK
-      </h1>
+      <div className="flex-1 flex items-center h-full">
+        <div className="flex items-center justify-center mt-2 ml-3">
+          <Image
+            src={"/images/logo2.png"}
+            alt="Profile"
+            width={100}
+            height={100}
+            className="object-contain"
+          />
+        </div>
+      </div>
       <div className="flex items-center gap-4">
         <button className="bg-[#A30000] p-2 text-[15px] rounded-sm text-white">
           Get in touch
@@ -51,73 +61,23 @@ const Navbar = () => {
               animate={{ opacity: 1, height: "200px" }}
               exit={{ opacity: 0, height: 0 }}
               transition={{ duration: 0.4, ease: "easeInOut" }}
-              className="absolute top-12 right-4 w-40 flex flex-col bg-[#A30000] rounded shadow-lg sm:hidden overflow-hidden"
+              className="absolute top-12 w-40 flex flex-col bg-[#000000]/90 rounded shadow-lg sm:hidden overflow-hidden"
             >
-              <a
-                href="#home"
-                className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]"
-              >
-                Home
-              </a>
-              <a
-                href="#about"
-                className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]"
-              >
-                About
-              </a>
-              <a
-                href="#projects"
-                className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]"
-              >
-                Projects
-              </a>
-              <a
-                href="#services"
-                className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]"
-              >
-                Services
-              </a>
-              <a
-                href="#stack"
-                className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]"
-              >
-                Stack
-              </a>
+              <Link href="/" className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]">Home</Link>
+              <Link href="#about" className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]">About</Link>
+              <Link href="#projects" className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]">Projects</Link>
+              <Link href="#services" className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]">Services</Link>
+              <Link href="#stack" className="text-white text-[15px] font-bebas px-4 py-2 hover:bg-[#870000]">Stack</Link>
             </motion.nav>
           )}
         </AnimatePresence>
         {/* Desktop nav links */}
-        <nav className="hidden sm:flex items-center gap-3">
-          <a
-            href="#home"
-            className="text-[15px] text-[#A30000] font-bebas hover:underline"
-          >
-            Home
-          </a>
-          <a
-            href="#about"
-            className="text-[15px] text-[#A30000] font-bebas hover:underline"
-          >
-            About
-          </a>
-          <a
-            href="#projects"
-            className="text-[15px] text-[#A30000] font-bebas hover:underline"
-          >
-            Projects
-          </a>
-          <a
-            href="#services"
-            className="text-[15px] text-[#A30000] font-bebas hover:underline"
-          >
-            Services
-          </a>
-          <a
-            href="#stack"
-            className="text-[15px] text-[#A30000] font-bebas hover:underline"
-          >
-            Stack
-          </a>
+        <nav className="hidden sm:flex items-center gap-3 mr-5">
+          <Link href="/" className="text-[15px] text-[#A30000] font-bebas hover:underline">Home</Link>
+          <Link href="#about" className="text-[15px] text-[#A30000] font-bebas hover:underline">About</Link>
+          <Link href="#projects" className="text-[15px] text-[#A30000] font-bebas hover:underline">Projects</Link>
+          <Link href="#services" className="text-[15px] text-[#A30000] font-bebas hover:underline">Services</Link>
+          <Link href="#stack" className="text-[15px] text-[#A30000] font-bebas hover:underline">Stack</Link>
         </nav>
       </div>
     </div>
