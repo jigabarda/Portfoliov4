5c933c6d4190ee092f14e86b5b29e21f73fb2220
James Ivan Gabarda <jamesivangabarda8@gmail.com>
2025-11-13 19:03:27 +0800

    update all files and configuration to the latest veresion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "udemy-certificate.s3.amazonaws.com",
+      "img-c.udemycdn.com"
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
