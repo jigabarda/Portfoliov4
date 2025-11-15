a909dcf66a77b447ac3fb140c832b9b3542cf5db
James Ivan Gabarda <jamesivangabarda8@gmail.com>
2025-11-15 20:45:43 +0800

    add new domain to insert svg

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["udemy-certificate.s3.amazonaws.com", "img-c.udemycdn.com"],
+    domains: [
+      "udemy-certificate.s3.amazonaws.com",
+      "img-c.udemycdn.com",
+      "cdn-icons-png.flaticon.com",
+    ],
   },
 };
 
