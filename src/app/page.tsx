07c5eb0a8dfad6777947af2441a583b052a4fa4a
James Ivan Gabarda <jamesivangabarda8@gmail.com>
2025-11-15 22:09:03 +0800

    update navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,32 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Stack from "./pages/Stack";
-import About from "./pages/About";
+import Navbar from "./about/page";
+import Home from "./home/page";
+import Projects from "./projects/page";
+import Stack from "./stacks/page";
+import About from "./about/page";
 import Footer from "./components/Footer";
 
 function Page() {
   return (
-    <div className="bg-[#0B0B0B]">
-      <div>
-        <Navbar />
-      </div>
+    <div className="bg-[#0B0B0B] scroll-smooth">
+      <Navbar />
+
       <div id="home">
         <Home />
       </div>
+
       <div id="projects">
         <Projects />
       </div>
+
       <div id="stack">
         <Stack />
       </div>
+
       <div id="about">
         <About />
       </div>
+
       <div id="footer">
         <Footer />
       </div>
